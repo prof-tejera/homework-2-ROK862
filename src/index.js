bdf887d0abe7b0e43318bc25cad251dc64cd133d
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 20:47:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './explosions.scss';
+import './colorsAnimation.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -16,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+© 2021 Gi
