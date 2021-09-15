7aa8f65701d786af487ecc308263a217c76c1f2b
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 19:54:42 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="pyro">
+          <div class="before"></div>
+          <div class="after"></div>
+        </div>
         <p>Hello CSCI E-39!</p>
         <p>
           My name is Robbins Jefferson Kariseb
